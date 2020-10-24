83fe3239c93ed7624b386f120c7a621dd96ee767
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-24 21:07:18 +0200

    Stage Punteggio Fixing

--- JS/main.js
+++ JS/main.js
@@ -1,44 +1,41 @@
-// GENERAZIONE BOMBE (16 numeri casuali non ripetibili)
-// creare un array vuota e una funzione generatore numeri
+// lista 16 numeri casuali
+var listaNumeriCasuali = [];
+var size = 5;
 
-var bombe16 = [];
-
-// creazione 16 numeri bombe
-for( var i = 0; i <= 3; i++) {
-    var bombeCasuali = generaNumeriCasuali(1, 100);
-    if (!bombe16.includes(bombeCasuali) ) {
-        bombe16.push(bombeCasuali);
+while (listaNumeriCasuali.length < size) {
+    var numeroCasuale = generaNumeri(1, 20);
+    if (!listaNumeriCasuali.includes(numeroCasuale)) {
+        listaNumeriCasuali.push(numeroCasuale);
     }
-    console.log(bombe16);
+}
+console.log(listaNumeriCasuali);
+
+
+// lista numeri inseriti dall'utente
+var numeriUtente = [];
+// finchè il numero sarà diverso, tra 1 e 100 e non compreso nell'array, devo pusharlo all'interno dell'array
+
+for (var i = 0; i < 10; i++) {
+    var domanda = parseInt(prompt("inserire un numero tra 1 e 20"))
+    while(domanda < 1 || domanda > 20 || isNaN(domanda)) {
+        alert("inserisci un numero nel range giusto!");
+        break;
+    }
+     if (listaNumeriCasuali.includes(domanda)) {
+        alert("hai attivato una bomba, hai perso!");
+        alert(numeriUtente.length + " tentativi riusciti")
+        break;
+    } else  if (!numeriUtente.includes(domanda) && !isNaN(domanda) && domanda > 0 && domanda < 20) {
+        alert("campo libero, procedi!");
+        numeriUtente.push(domanda);
+        console.log(domanda);
+    }
 }
 
-// CHIEDERE ALL'UTENTE DI INSERIRE 84 NUMERI NON RIPETIBILI TRA 1 E 100
-var arrayNumeriUtente = [];
 
-for ( var i = 0; i < 84; i++) {
-    var numeroUtente = parseInt (prompt ("inserisci un numero tra 1 e 100" ) );
-    while (arrayNumeriUtente.includes(numeroUtente)){
-        alert("numero già inserito, inseriscine un altro!");
-        break
-    }
-    if ( numeroUtente > 100 || numeroUtente < 1 ) {
-        alert("scegli numero tra il range!");
-    }  else if (bombe16.includes(numeroUtente)) {
-        alert("bomba attivata, hai perso!");
-        alert("il tuo punteggio è: " + arrayNumeriUtente.length);
-        break
-    } else if (!bombe16.includes(numeroUtente) ) {
-        arrayNumeriUtente.push(numeroUtente);
-        alert("inserisci prossimo numero");
-    }
- 
+
+// funzione per la creazione numeri casuali
+function generaNumeri (max,min) {
+    var risultato = (Math.floor(Math.random ()* (max-min) + 1) + min);
+    return risultato;
 }
-
-
-// FUNZIONI
-
-function generaNumeriCasuali (min, max) {
-    return Math.floor(Math.random() * ((max - min) +1) + min)
-}
-var numero = generaNumeriCasuali(1, 10);
-console.log(numero);
