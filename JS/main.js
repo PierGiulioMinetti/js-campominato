1fcba0664320e6f3a45ac285accd134a4c3f53ea
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-25 19:11:17 +0100

    Bonus Done!

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,46 @@
+// BONUS: (da fare solo se funziona tutto il resto)
+// all'inizio il software richiede anche una difficoltà all'utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
+// creare uno switch o un if
+// cambia il range dei numeri diminuendolo
+
+var max ;
+var difficolta = parseInt (prompt ("scegli la difficoltà inserendo numeri da 1 a 3;\n16 bombe sparse sul campo;\n \n difficolta 1 : 100 numeri; \ndifficolta 2 : 80 numeri; \ndifficolta 3 : 50 numeri;") );
+
+if (difficolta == 1) {
+    max = 100;
+} else if (difficolta == 2) {
+    max = 80;
+} else if (difficolta == 3) {
+    max = 50;
+}
+
+// switch(difficolta) {
+//     case "difficoltà 1":
+//       max = 100;
+//       break;
+//     case "difficoltà 2":
+//         max = 80;
+//       break;
+//       case "difficoltà 3":
+//         max = 50;
+//         break;
+//     default:
+//       alert("inserisci : 'difficoltà + numero difficoltà scelta'");
+//   }
+
+
+
+
 
 // LISTA BOMBE
 var listaBombe = [];
 
-while (listaBombe.length < 2  ) {
-    var number = numberGenerator(1,10);
+while (listaBombe.length < 16  ) {
+    var number = numberGenerator(1,max);
     if (!listaBombe.includes(number)) {
         listaBombe.push(number);
     }
@@ -13,8 +50,8 @@ console.log(listaBombe);
 // NUMERI UTENTE
 var numeriUtente = [];
 
-for (var i = 0; numeriUtente.length < 8; i++) {
-    var inserisciNumero = parseInt (prompt ("inserisci un numero tra 1 e 10"));
+for (var i = 0; numeriUtente.length < 84; i++) {
+    var inserisciNumero = parseInt (prompt ("inserisci un numero tra 1 e 100"));
 
     if (listaBombe.includes(inserisciNumero)) {
         alert(" bomba attivata! hai perso!");
